5965204d77e62226829a73e9ed7e037c9a2ddda7
adrian <jason@lumiterra.net>
2024-10-08 19:39:06 +0800

    add test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,32 +7,5 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const blockTime = 1717200000;
-    await sendFeiShuNotificationWithPost(
-        {
-            title: "❌ Lumi Layer3 Rollup 出块异常",
-            content: [
-             [
-                {
-                    "tag": "text",
-                    "text": "上次出块时间：" + dayjs(blockTime * 1000).format('YYYY-MM-DD HH:mm:ss')
-                },
-                {
-                    "tag": "a",
-                    "text": "\nGitHub Issue",
-                    "href": "https://github.com/LumiterraCommunity/status-pages/issues"
-                }
-             ]
-            ]
-        }
-    );
-
-    await sendFeiShuNotificationWithText(
-        JSON.stringify(req.query)
-    );
-    
     res.send("hello,lumiterra");
-
-    console.log("message:",req.query)
-    console.log("env value:",process.env.FEISHU_SECRET_TEST)
 }
